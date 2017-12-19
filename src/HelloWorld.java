6601a29cc3d63e3db79f6844b4beaaf8393c6424
Van <vandebergz@gmail.com>
2017-12-19 15:20:13 +0700

    Edited the java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main (String [] args) {
-		System.out.println("Hello World again");
-		System.out.println("Hae!");
+		System.out.println("Hello, World!");
+		System.out.println("Hi! It's a me, Mario");
 	}
 }
